382e10d1ba4b379c699311c65f4edf1a1f963876
Armaan Rathi <armaanrathi84@gmail.com>
2026-08-15 16:09:15 +0100

    added web analytics

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import { DataProvider } from "./data/DataContext";
 import { App } from "./App";
 import "./styles.css";
@@ -12,5 +13,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <App />
       </DataProvider>
     </BrowserRouter>
+    <Analytics />
   </React.StrictMode>
 );
